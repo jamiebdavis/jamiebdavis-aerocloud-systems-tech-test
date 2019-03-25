1597a49de678e170fead2894f2c543b662db2537
jamiebdavis <jamied1985@gmail.com>
2019-03-25 12:18:43 +0000

    removed default from app.js - added components folder and toolbar component

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Toolbar = () => {
+  return (
+    <header>
+      <p>Logo</p>
+      <p>Title</p>
+      <p>Reset Button</p>
+    </header>
+  );
+};
+
+export default Toolbar;
