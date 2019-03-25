7cf92e127a81f099ba167f97a44a2bdbc81ede26
jamiebdavis <jamied1985@gmail.com>
2019-03-25 12:45:29 +0000

    added dummy data and state  to app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 import Toolbar from "./components/Toolbar/Toolbar";
 import "./App.css";
 
+const completeHotelList = [
+  { name: "hotelone", starRating: 5, facilities: ["car park", "pool"] },
+  { name: "hoteltwo", starRating: 3, facilities: ["car park", "gym"] },
+  { name: "hotelthree", starRating: 3, facilities: [] },
+  { name: "hotelfour", starRating: 2, facilities: ["pool"] },
+  { name: "hotelfive", starRating: 1, facilities: ["gym"] }
+];
+
 class App extends Component {
+  state = {
+    hotels: completeHotelList,
+    searchInput: ""
+  };
   render() {
     return (
       <div className="App">
-        <p>App content</p>
         <Toolbar />
       </div>
     );
