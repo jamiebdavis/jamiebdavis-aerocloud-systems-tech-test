3d88a4496ed993e88d50db2adab9b3f940005b79
jamiebdavis <jamied1985@gmail.com>
2019-03-25 12:42:30 +0000

    added test to show button component renders in toolbar

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import Toolbar from "./components/Toolbar/Toolbar";
+import Button from "./components/Button/Button";
 
 configure({ adapter: new Adapter() });
 
@@ -12,5 +13,13 @@ describe("<App />", () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper.find(Toolbar)).toHaveLength(1);
+  });
+});
+
+describe("<Toolbar />", () => {
+  it("Containers <Button /> component", () => {
+    const wrapper = shallow(<Toolbar />);
+
+    expect(wrapper.find(Button)).toHaveLength(1);
   });
 });
