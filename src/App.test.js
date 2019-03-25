4e93ba80b43cc6e55b38fda675ba222a656e7fc3
jamiebdavis <jamied1985@gmail.com>
2019-03-25 12:32:02 +0000

    added test to see if toolbar component renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from "./App";
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Toolbar from "./components/Toolbar/Toolbar";
+
+configure({ adapter: new Adapter() });
+
+describe("<App />", () => {
+  it("Containers <Toolbar /> component", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(Toolbar)).toHaveLength(1);
+  });
 });
